0f4a2a9027f3e174a39be00fb1644f078fd441da
RohitRox <link2j.roy@gmail.com>
2015-05-10 12:35:24 -0400

    minor css and js syntax fix

--- vendor/assets/javascripts/controllers/postsCtrl.js
+++ vendor/assets/javascripts/controllers/postsCtrl.js
@@ -1,5 +1,5 @@
 this.app.controller('postsCtrl', [
-  '$scope', 'Posts', '$log', function($scope, Posts, $log,) {
+  '$scope', 'Posts', '$log', function($scope, Posts, $log) {
     return Posts.query().$promise.then(function(data) {
       $scope.post = data;
     });
